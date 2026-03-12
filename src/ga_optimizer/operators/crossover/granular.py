aa85bef89669dda525340e3c9a890f4c78bd940b
Krystian Węgrzyn <krystianwegrzyn@interia.pl>
2026-03-12 22:43:21 +0100

    Dodano krzyżowanie

--- src/ga_optimizer/operators/crossover/granular.py
+++ src/ga_optimizer/operators/crossover/granular.py
@@ -1,1 +1,50 @@
-# Implementuje niestandardowe krzyżowanie ziarniste
+# krzyżowanie ziarniste
+
+from __future__ import annotations
+
+import random
+from typing import Any
+
+
+def granular_crossover(
+    chromosomes: list[list[int]],
+    config_dict: dict[str, Any],
+) -> list[list[int]]:
+
+    p = config_dict["crossover_granular_p"]
+    granularity = config_dict["crossover_granular_granularity"]
+
+    new_population = [c[:] for c in chromosomes]
+
+    for i in range(0, len(chromosomes) - 1, 2):
+
+        parent1 = chromosomes[i]
+        parent2 = chromosomes[i + 1]
+
+        # czy w ogóle robimy crossover
+        if random.random() > p:
+            continue
+
+        length = len(parent1)
+
+        child1 = parent1[:]
+        child2 = parent2[:]
+
+        # przechodzimy blokami genów
+        for g in range(0, length, granularity):
+
+            a = random.random()
+
+            end = min(g + granularity, length)
+
+            if a <= 0.5:
+                child1[g:end] = parent1[g:end]
+                child2[g:end] = parent2[g:end]
+            else:
+                child1[g:end] = parent2[g:end]
+                child2[g:end] = parent1[g:end]
+
+        new_population[i] = child1
+        new_population[i + 1] = child2
+
+    return new_population
