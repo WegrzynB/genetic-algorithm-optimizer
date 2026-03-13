6ce89a65bd6db5bfa8c9cc66ff05351a3896c3f8
CptJarkov <kz.jarkowski@gmail.com>
2026-03-13 19:08:27 +0100

    Rollback of changes to lifecycle.py, changed dispatch_selection.py to reflect the rollback.

--- src/ga_optimizer/operators/dispatch/dispatch_selection.py
+++ src/ga_optimizer/operators/dispatch/dispatch_selection.py
@@ -1,4 +1,4 @@
-# ga_optimizer/operators/selection/dispatch_selection.py
+# ga_optimizer/operators/dispatch/dispatch_selection.py
 
 from __future__ import annotations
 from typing import Any
@@ -11,28 +11,35 @@ from ga_optimizer.operators.selection.tournament import select_tournament
 
 
 def dispatch_selection(
-    population: Population,
+    chromosomes: list[list[int]],
     config_dict: dict[str, Any],
 ) -> list[list[int]]:
     
+    temp_pop = Population(config_dict=config_dict)
+    
+    # Wrzucenie genów automatycznie uruchamia ewaluację (liczy fitness)
+    temp_pop.set_chromosomes(chromosomes)
+    # ------------------------------------
+
     method_name = config_dict.get("selection_method", "roulette")
-    num_parents = len(population)
+    num_parents = len(chromosomes)
     
     match method_name:
         case "best":
-            selected_chromosomes = select_best(population, num_parents, config_dict)
+            # Przekazujemy naszą tymczasową (ale w pełni przeliczoną) populację
+            selected_chromosomes = select_best(temp_pop, num_parents, config_dict)
             
         case "roulette":
-            selected_chromosomes = select_roulette(population, num_parents, config_dict, is_copy=False)
+            selected_chromosomes = select_roulette(temp_pop, num_parents, config_dict, is_copy=False)
             
         case "roulette copy":
-            selected_chromosomes = select_roulette(population, num_parents, config_dict, is_copy=True)
+            selected_chromosomes = select_roulette(temp_pop, num_parents, config_dict, is_copy=True)
 
         case "tournament":
-            selected_chromosomes = select_tournament(population, num_parents, config_dict)
+            selected_chromosomes = select_tournament(temp_pop, num_parents, config_dict)
 
         case _:
             print(f"[Ostrzeżenie] Nieznana metoda selekcji: {method_name}. Przepisuję populację 1:1.")
-            selected_chromosomes = [chrom.copy() for chrom in population.chromosomes]
+            selected_chromosomes = [chrom.copy() for chrom in chromosomes]
 
     return selected_chromosomes
